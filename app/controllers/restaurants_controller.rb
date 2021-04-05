c4bec3a8203113fe9bdc5a801f59cc8fab6e2c54
omar rachedi <omartchik@gmail.com>
2021-04-06 00:15:49 +0200

    voila c'est fini le crud hahahah

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,41 +1,46 @@
 class RestaurantsController < ApplicationController
+# LISTE DES RESTAURANTS
   def index
-    @restaurants = Restaurant.all
+  @restaurants = Restaurant.all
+end
+# DETAILS DU RESTAURANT
+def show
+  @restaurant = Restaurant.find(params[:id])
+end
 
-  end
-  def new
-    @restaurant = Restaurant.new
-  end
-
-  def create
-    @restaurant = Restaurant.create(restaurant_params)
-    @restaurant.save
-    redirect_to(restaurants_path(@restaurant))
-  end
-
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
+# EDITER UN RESTAURANT
 def edit
   @restaurant = Restaurant.find(params[:id])
 end
-
+# MISE A JOUR
 def update
   @restaurant = Restaurant.find(params[:id])
   @restaurant.update(restaurant_params)
-  redirect_to(restaurants_path(@restaurant))
+  redirect_to(restaurants_path)
 end
+# NOUVEAU RESTAURANT
+def new
+  @restaurant = Restaurant.new
+end
+# CREATION DU RESTAURANT
+def create
+  @restaurant = Restaurant.create(restaurant_params)
+  @restaurant.save
+  redirect_to(restaurants_path)
+end
+# SUPPRIMER UN RESTAURANT
 def destroy
   @restaurant = Restaurant.find(params[:id])
   @restaurant.destroy
-  redirect_to restaurants_path
+  redirect_to(restaurants_path)
 end
 
 
-
-  private
+# PRIVATE
+private
 def restaurant_params
   params.require(:restaurant).permit(:name, :rating, :address)
 end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # la page d'accueil
-  resources :restaurants
-  root to: "pages#home"
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+resources :restaurants
 end
