6c86535205e0e8b8b6c93a691a8fcdba5845ef8f
omar rachedi <omartchik@gmail.com>
2021-04-05 23:40:04 +0200

    CRUD implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # la page d'accueil
+  resources :restaurants
   root to: "pages#home"
-  get 'restaurants', to: "restaurants#index"
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'omar rachedi', rating: 4, address: "39 RUE JEAN MARTIN MARSEILLE")
+Restaurant.create(name: 'Aliane Baya', rating: 5, address: "39 RUE HENRI GONNARD MARSEILLE")
+Restaurant.create(name: 'Lyes Kaci', rating: 4, address: "54 RUE SENAC MARSEILLE")
