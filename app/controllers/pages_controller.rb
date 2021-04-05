213fe9da84ad31c543821105dd71c755c94592e2
omar rachedi <omartchik@gmail.com>
2021-04-05 18:39:35 +0200

    ajouter un model restaurant + rails db:migrate

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @membres = ["omar", "baya", 'hamid', 'lyes', 'djamil', 'aurora','bahus']
-
-    if params[:membre]
-      @membres = @membres.select {|membre| membre.start_with?(params[:membre])}
-    end
+  
 
   end
 
