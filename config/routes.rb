a11ab9696ae2127137a0a5c8a69b3a724c6f0275
omar rachedi <omartchik@gmail.com>
2021-04-05 18:52:15 +0200

    j'ai crée la base de donnée pour les restaurants avec le controlleur correspodant et la page index qui sert de vue de tous les restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # la page d'accueil
   root to: "pages#home"
+  get 'restaurants', to: "restaurants#index"
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 end
